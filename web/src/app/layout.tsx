89f94a976a91c4b61bf4a51662c56188b9776cb0
Lost <pandagaminggr@gmail.com>
2025-04-18 20:30:21 +0300

    feat: remove api; update deps; add prisma

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,21 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
-	);
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "(DEV) Signs App",
+  description: "(DEV) Modernized signs app for the Greek Driving Test",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
