435751c535b9e6aac471e2678cd5badf038b3955
Lost <pandagaminggr@gmail.com>
2025-04-20 12:52:24 +0300

    chore: import prettier; fix eslintConfig type issue

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,20 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "(DEV) Signs App",
-  description: "(DEV) Modernized signs app for the Greek Driving Test",
+	title: "(DEV) Signs App",
+	description: "(DEV) Modernized signs app for the Greek Driving Test",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className="">{children}</body>
+		</html>
+	);
 }
